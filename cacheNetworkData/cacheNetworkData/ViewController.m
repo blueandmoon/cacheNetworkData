361d5ab4b9a72c2f30d03403f7225cfa465e1728
李根 <1227397225@qq.com>
2016-07-08 16:04:38 +0800

    no message

---
+++ cacheNetworkData/cacheNetworkData/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  cacheNetworkData
+//
+//  Created by 李根 on 16/7/7.
+//  Copyright © 2016年 ligen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#define DATAURL @"http://wapi.go2family.com/api/APIAppIndex/GetAppIndexSeviceListByIOS?CityID=320500&AuthoLevel=2&UserID=18112572968"
+
+#import "NetWorkingTool.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
